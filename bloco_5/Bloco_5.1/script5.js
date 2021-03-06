427c42a42943389ba197a00b3fc2ffd39608b60e
Cristian <cristian.bugs@gmail.com>
2021-03-06 03:22:12 -0300

    exercicios 5.1

--- bloco_5/Bloco_5.1/script5.js
+++ bloco_5/Bloco_5.1/script5.js
@@ -1,11 +1,31 @@
 function changeText() {
     let paragraph = document.getElementsByTagName('p')[1];
-    paragraph.innerHTML = "Ganhar 3.500 e estar em um empresa cabulosa!!"
+    paragraph.innerHTML = "Estar feliz em um bom emprego"
   }
   changeText ()
 
-function changeText() {
-    let paragraph = document.getElementsByTagName('p')[1];
-    paragraph.innerHTML = "Ganhar 3.500 e estar em um empresa cabulosa!!"
+function ToGreen() {
+    let Color = document.getElementsByClassName('main-content')[0];
+    Color.style.background = "rgb(76,164,109)";
   }
-changeText ()
+  ToGreen ()
+
+function SquareRedToWhite() {
+    let WhiteSquare = document.getElementsByClassName("center-content")[0];
+    WhiteSquare.style.background = "white";
+  }
+  SquareRedToWhite ()
+
+  function FirstH() {
+    let eight = document.getElementsByTagName("h1")[0];
+    eight.innerText= "New Text";
+  }
+  FirstH ()
+
+  function Up () {
+    let Upper = document.getElementsByTagName("p")[0, 1];
+    Upper.innerHTML = Upper.innerHTML.toUpperCase();
+  }
+  Up ()
+
+  console.log(document.getElementsByTagName("p"))
